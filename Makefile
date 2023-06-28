0525d08d7659ac9483680fcb86f2ace00e7c4a19
hoshi005 <hoshi005@me.com>
2023-06-28 19:49:07 +0900

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+generate_icon_files:
+	fvm flutter pub run flutter_launcher_icons:main
---
+++ android/app/src/main/kotlin/jp/co/shlab/map_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package jp.co.shlab.map_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
